5dd54592a87db68876c173f562f0ec2f6ae49920
jeroendedauw <jeroendedauw@gmail.com>
2013-09-08 07:03:00 +0200

    Improve script

--- build.sh
+++ build.sh
@@ -2,15 +2,21 @@
 
 set -x
 
+rm -rf QueryrAPI
+git clone git@github.com:JeroenDeDauw/QueryrAPI.git
+cd QueryrAPI
+
+npm install
+
 while true
 do
-    git clone git@github.com:JeroenDeDauw/QueryrAPI.git
-    cd QueryrAPI
-
-    npm install
     node api &
 
     sleep 120
+
+    git pull
+    npm update
+
     kill -9 $(ps -ef |grep `whoami`| grep 'node api' | cut -c10-15 | head -n1) 
 done
 
